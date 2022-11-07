6b4ea252524b2ab8d7ac65d54bca44f296060460
Bonnie Owens <arlolocos@gmail.com>
2022-11-07 15:11:06 -0500

    Completed word count

--- wordcount.py
+++ wordcount.py
@@ -2,3 +2,23 @@
 
 
 # put your code here.
+def count_words(filename):
+    count = {}
+    input_file = open(filename)
+    for line in input_file:
+        words = line.split(" ")
+        for word in words:
+            if word.strip() in count:
+                count[word.strip()] +=1
+            else:
+               count [word.strip()]=1
+
+    for key, value in count.items():
+        print(key, value)
+
+    return count
+
+
+results = count_words('twain.txt')
+
+# print(results)
