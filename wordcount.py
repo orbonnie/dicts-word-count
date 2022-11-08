84a181af421bda2b31035599b30a4e2664e33660
Bonnie Owens <arlolocos@gmail.com>
2022-11-07 20:59:16 -0500

    refactored to remove non alphanueric characters

--- wordcount.py
+++ wordcount.py
@@ -1,20 +1,24 @@
-"""Count words in file."""
+import re
 
-
-# put your code here.
 def count_words(filename):
+    """Count words in file."""
+
     count = {}
     input_file = open(filename)
     for line in input_file:
-        words = line.split(" ")
+        line = re.sub(r"[^\w\s]", "", line).lower()
+        words = line.rstrip().split(" ")
         for word in words:
-            if word.strip() in count:
-                count[word.strip()] +=1
-            else:
-               count [word.strip()]=1
+            if word:
+                if word.strip() in count:
+                    count[word.strip()] += 1
+                else:
+                    count [word.strip()] = 1
 
-    for key, value in count.items():
-        print(key, value)
+    sorted_count = sorted(count.items(), key=lambda item: item[1])
+
+    for value in sorted_count:
+        print(value[0], value[1])
 
     return count
 
